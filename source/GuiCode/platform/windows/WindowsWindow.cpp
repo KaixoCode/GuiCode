a1d7593000c9943da90e4ab7cb7251f41794f32c
KaixoCode <jeroenzwiers2@gmail.com>
2021-03-10 16:59:40 +0100

    Revert "COMPLETE FAILURE"
    This reverts commit 73133afed36952f60d419e718f2f618ffec18dc8.

--- source/GuiCode/platform/windows/WindowsWindow.cpp
+++ source/GuiCode/platform/windows/WindowsWindow.cpp
@@ -549,11 +549,11 @@ void WindowsWindow::Icon(int id)
     //images[0].width = a.width;
     //images[0].height = a.height;
 
+    m_IconTexture.SetTexture(m_hWindowIcon);
 
     m_hWindowIcon = LoadIcon(GetModuleHandle(NULL), MAKEINTRESOURCE(id));
     SendMessage(GetWin32Handle(), WM_SETICON, ICON_BIG, (LPARAM)m_hWindowIcon);
     SendMessage(GetWin32Handle(), WM_SETICON, ICON_SMALL, (LPARAM)m_hWindowIcon);
-    m_IconTexture.SetTexture("./" + m_Name + ".png");
 
 
     //glfwSetWindowIcon(*this, 1, images);
