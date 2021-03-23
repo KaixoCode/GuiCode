efd63779cde11419816463b4a395c3782a61943f
KaixoCode <jeroenzwiers2@gmail.com>
2021-03-23 18:26:21 +0100

    Update Graphics.cpp

--- source/GuiCode/ui/graphics/Graphics.cpp
+++ source/GuiCode/ui/graphics/Graphics.cpp
@@ -500,7 +500,7 @@ namespace Graphics
             "    float r = (pow(pos.x - x, 2) / pow(dimensions.z / 2, 2)) + (pow(pos.y - y, 2) / pow(dimensions.w / 2, 2)); "
             "    if (aa > aend) { discard; } "
             "    else if (r > 1) { discard; } "
-            "    else if (r > 0.95) { FragColor = vec4(color.rgb, (0.5 / r) * color.a); } "
+            "    else if (r > 0.9) { FragColor = vec4(color.rgb, (0.5 / r) * color.a); } "
             "    else { FragColor = color; } "
             "} "
         };
