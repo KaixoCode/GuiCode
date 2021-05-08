b063242b9ba4ca9a910c5f47d6a5868b3e63e11a
KaixoCode <jeroenzwiers2@gmail.com>
2021-05-08 23:56:37 +0200

    Update Vec.hpp

--- include/GuiCode/data/Vec.hpp
+++ include/GuiCode/data/Vec.hpp
@@ -61,6 +61,7 @@ struct Vec3
     Vec3& operator =(const Vec3& other) { x = other.x, y = other.y; z = other.z; return *this; }
     Vec3& operator+=(const Vec3& other) { x += other.x; y += other.y; z += other.z; return *this; }
     Vec3& operator-=(const Vec3& other) { x -= other.x; y -= other.y; z -= other.z; return *this; }
+    bool operator==(const Vec3& other) const { return x == other.x && y == other.y && z == other.z; }
 
     operator glm::vec3() const { return { x, y, z }; }
 
